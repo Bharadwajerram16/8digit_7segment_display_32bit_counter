8eed4b445c904f169b0d893b4f5a265eb8825490
Bharadwaj <bharadwajcollege@gmail.com>
2026-04-01 18:04:18 +0530

    Updated 7 segment logic and improved counter design

--- Eight_digit_Display.v
+++ Eight_digit_Display.v
@@ -1,20 +1,12 @@
-module bcd_to_7segment_bh_tb();
-reg [3:0] bcd;
-wire [6:0] seg;
-10
-bcd_to_7segment_bh x1(seg,bcd);
-initial
-begin
-bcd=4'b0000;
-#10 bcd=4'b0001;
-#10 bcd=4'b0010;
-#10 bcd=4'b0011;
-#10 bcd=4'b0100;
-#10 bcd=4'b0101;
-#10 bcd=4'b0110;
-#10 bcd=4'b0111;
-#10 bcd=4'b1000;
-#10 bcd=4'b1001;
-#10 $stop;
-end
+module bcd_to_7seg_8digit_st(seg_out,bcd_in);
+input [31:0]bcd_in;
+output [55:0]seg_out;
+bcd_to_7segment_bh s0(seg_out[6:0],bcd_in[3:0]);
+bcd_to_7segment_bh s1(seg_out[13:7],bcd_in[7:4]);
+bcd_to_7segment_bh s2(seg_out[20:14],bcd_in[11:8]);
+bcd_to_7segment_bh s3(seg_out[27:21],bcd_in[15:12]);
+bcd_to_7segment_bh s4(seg_out[34:28],bcd_in[19:16]);
+bcd_to_7segment_bh s5(seg_out[41:35],bcd_in[23:20]);
+bcd_to_7segment_bh s6(seg_out[48:42],bcd_in[27:24]);
+bcd_to_7segment_bh s7(seg_out[55:49],bcd_in[31:28]);
 endmodule
